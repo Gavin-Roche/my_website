5ff2cb2cdb8ea14557f03c4b3108207e52656f31
Gavin-Roche <cubergr@gmail.com>
2025-02-06 18:50:03 +0000

    Breakpoint flicker bug fix

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,28 @@
 const hamburgerMenu = document.querySelector('.hamburgerMenu');
-const NavMenu = document.querySelector('nav ul');
+const navLinks = document.querySelector('nav ul');
 
 hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('open');
-    NavMenu.classList.toggle('open');
+    // Check if the menu is open or closed
+    if (navLinks.classList.contains('open')) {
+        // Triggers the closing animation
+        navLinks.classList.remove('open');
+        hamburgerMenu.classList.remove('open');
+
+        // After 500ms (duration of the CSS transition), set visibility to hidden this is to stop breakpoint flicker
+        setTimeout(() => {
+            navLinks.style.visibility = 'hidden';
+        }, 500);
+    } else {
+        // Triggers the opening animation
+        navLinks.style.visibility = 'visible';
+        navLinks.classList.add('open');
+        hamburgerMenu.classList.add('open');
+    }
+});
+
+// Ensures that the navBar is not invisible in desktop
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1000) {
+        navLinks.style.visibility = '';
+    }
 });
