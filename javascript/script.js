6dfbe7a2875d15ad38a955a9087c7c5db487a7f9
Gavin-Roche <cubergr@gmail.com>
2025-02-08 13:14:08 +0000

    responsive navbar with fixed position (desktop) & no scroll (mobile)
    The navbar is now fixed in place on desktop and fades to black on scroll. Scrolling is not allowed when the navbar is open on mobile.

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,49 @@
 const hamburgerMenu = document.querySelector('.hamburgerMenu');
-const navLinks = document.querySelector('nav ul');
+const navList = document.querySelector('nav ul');
+const nav = document.querySelector('nav');
+const body = document.body
 
 hamburgerMenu.addEventListener('click', () => {
     // Check if the menu is open or closed
-    if (navLinks.classList.contains('open')) {
-        // Triggers the closing animation
-        navLinks.classList.remove('open');
+    if (navList.classList.contains('open')) {
+        // Triggers the closing animation and disables scrolling
+        navList.classList.remove('open');
         hamburgerMenu.classList.remove('open');
+        body.classList.remove('noScroll');
 
         // After 500ms (duration of the CSS transition), set visibility to hidden this is to stop breakpoint flicker
         setTimeout(() => {
-            navLinks.style.visibility = 'hidden';
+            navList.style.visibility = 'hidden';
         }, 500);
     } else {
-        // Triggers the opening animation
-        navLinks.style.visibility = 'visible';
-        navLinks.classList.add('open');
+        // Triggers the opening animation and enables scrolling
+        navList.style.visibility = 'visible';
+        navList.classList.add('open');
         hamburgerMenu.classList.add('open');
+        body.classList.add('noScroll');
     }
 });
 
-// Ensures that the navBar is not invisible in desktop
+// Ensures that the navBar is not invisible in desktop and closes the NavBar when  you transition to desktop
 window.addEventListener('resize', () => {
     if (window.innerWidth > 1000) {
-        navLinks.style.visibility = '';
+        navList.style.visibility = '';
+
+        navList.classList.remove('open');
+        hamburgerMenu.classList.remove('open');
+        body.classList.remove('noScroll');
     }
 });
+
+// Fades in and out the navBar background-color
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        nav.classList.add('backgroundColor');
+    }
+    else{
+        nav.classList.remove('backgroundColor');
+    }
+
+});
+    
+
