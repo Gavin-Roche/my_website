5bd82f1af04e840ff9058c7c3fead6f9d9017600
Gavin-Roche <cubergr@gmail.com>
2025-02-22 18:40:39 +0000

    Added in Tab switcher for Accordion

--- javascript/about_me.js
+++ javascript/about_me.js
@@ -1,5 +1,6 @@
 const accordionHeaders = document.querySelectorAll('.accordionHeader');
 
+// Controls the accordion behavior
 accordionHeaders.forEach(header => {
     header.addEventListener('click', function() {
         // Closes all the other accordion's content
@@ -9,5 +10,30 @@ accordionHeaders.forEach(header => {
             }   
         });
         this.classList.toggle('active');
+    });
+});
+
+const tabLinks = document.querySelectorAll('.tabLinks');
+const years = document.querySelectorAll('.year');
+
+// Switches between the different tabs
+tabLinks.forEach((tab, index) => {
+    tab.addEventListener('click', function() {
+
+        // Removes active from all tabs
+        tabLinks.forEach(otherTab => {
+                otherTab.classList.remove('active');
+        });
+
+        // Adds active to a tab
+        this.classList.add('active')
+
+        // Removes active from tab content
+        years.forEach(year => {
+            year.classList.remove('active');
+        });
+
+        // Adds active to the corresponding content
+        years[index].classList.add('active');
     });
 });
